6bed2f578b4dda2c79b8c7f6269f8aeb10866517
ejay11 <ellen.jaquette@gmail.com>
2021-12-22 21:03:08 -0600

    added basic x and o classes

--- play.rb
+++ play.rb
@@ -6,6 +6,7 @@ class Board
     @line_two = ['2|   |   |   |']
     @line_three = ['3|   |   |   |']
   end
+
   # Puts Board to Terimal
   def show_board
     puts @line_zero
@@ -15,6 +16,32 @@ class Board
   end
 end
 
+# Class for O Pieces
+class Pieceoh
+  attr_accessor :array_num, :space_num
+
+  def initialize(array_num, space_num)
+    @array_num = array_num
+    @space_num = space_num
+  end
+end
+
+# Class for X Pieces
+class Piecex
+  attr_accessor :array_num, :space_num
+
+  def initialize(array_num, space_num)
+    @array_num = array_num
+    @space_num = space_num
+  end
+end
+
 # Run Game
 game = Board.new
 game.show_board
+
+turn_one = Pieceoh.new(2, 3)
+puts turn_one.array_num
+
+turn_two = Piecex.new(1, 3)
+puts turn_two.array_num
