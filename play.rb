78aae36705ae9c7b4a179a9bf3020cb3181d93eb
ejay11 <ellen.jaquette@gmail.com>
2021-12-26 20:23:30 -0600

    piece classes updated, placemend module created with errors

--- play.rb
+++ play.rb
@@ -18,21 +18,38 @@ end
 
 # Class for O Pieces
 class Pieceoh
-  attr_accessor :array_num, :space_num
-
+  attr_accessor :array_num, :space_num, :display
+  include Placement
   def initialize(array_num, space_num)
     @array_num = array_num
     @space_num = space_num
+    @display = 'O'
   end
 end
 
 # Class for X Pieces
 class Piecex
-  attr_accessor :array_num, :space_num
+  attr_accessor :array_num, :space_num, :display
 
   def initialize(array_num, space_num)
-    @array_num = array_num
-    @space_num = space_num
+    @array_num = array_num.to_i
+    @space_num = space_num.to_i
+    @display = 'X'
+  end
+end
+
+# Module for calculating array placement
+module Placement
+  def calclate_array(array_num)
+    insert_array = ''
+    if array_num == 1
+      insert_array = 'line_one'
+    elsif array_num == 2
+      insert_array = 'line_two'
+    else
+      insert_array = 'line_three'
+    end
+    insert_array
   end
 end
 
@@ -42,6 +59,11 @@ game.show_board
 
 turn_one = Pieceoh.new(2, 3)
 puts turn_one.array_num
+puts turn_one.space_num
+puts turn_one.display
+puts turn_one.calculate_array(2)
 
 turn_two = Piecex.new(1, 3)
 puts turn_two.array_num
+puts turn_two.space_num
+puts turn_two.display
