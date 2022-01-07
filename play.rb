d530512ac99213ce76604ac462e16e39dfcbf824
ejay11 <ellen.jaquette@gmail.com>
2022-01-06 20:32:29 -0600

    play method added to insert X or O

--- play.rb
+++ play.rb
@@ -1,31 +1,22 @@
 # Creates Board
-class Board
+class Game
   attr_accessor :row_zero, :row_one, :row_two, :row_three
 
-  # Creates Class Variables for all sub-classes to access (board)
-  # @@row_zero = [' | 1 | 2 | 3 |']
-  # @@row_one = ['1|   |   |   |']
-  # @@row_two = ['2|   |   |   |']
-  # @@row_three = ['3|   |   |   |']
-
   def initialize
-    @row_zero = [' | 1 | 2 | 3 |']
-    @row_one = ['1|   |   |   |']
-    @row_two = ['2|   |   |   |']
-    @row_three = ['3|   |   |   |']
+    @row_zero = [' ', '|', '1', '|', '2', '|', '3', '|']
+    @row_one = ['1', '|', ' ', '|', ' ', '|', ' ', '|']
+    @row_two = ['2', '|', ' ', '|', ' ', '|', ' ', '|']
+    @row_three = ['3', '|', ' ', '|', ' ', '|', ' ', '|']
   end
 
   # Puts Board to Terimal
   def show_board
-    puts @row_zero
-    puts @row_one
-    puts @row_two
-    puts @row_three
+    puts @row_zero.join(' ')
+    puts @row_one.join(' ')
+    puts @row_two.join(' ')
+    puts @row_three.join(' ')
   end
-end
 
-# Module for calculating array placement
-module Placement
   def calculate_array(array_num)
     if array_num == 1
       @row_one
@@ -36,61 +27,33 @@ module Placement
     end
   end
 
-  def calculate_space(space_num)
-    real_space = 0
+  def calculate_column(space_num)
+    column_space = 0
     if space_num == 1
-      real_space = 4
+      column_space = 2
     elsif space_num == 2
-      real_space = 8
+      column_space = 4
     elsif space_num == 3
-      real_space = 12
+      column_space = 6
     else
       puts 'Invalid row piece location, please enter 1, 2, or 3'
     end
-    puts "The array space is #{real_space}"
+    column_space
+  end
+
+  def play(marker, row_num, column_num)
+    puts "The column number is #{calculate_column(column_num)}"
+    puts "The array you'll need is #{calculate_array(row_num)}"
+    puts "The marker is #{marker}"
+    show_board
+    calculate_array(row_num).insert(calculate_column(column_num), marker)
+    calculate_array(row_num).delete_at(calculate_column(column_num)+1)
+    show_board
   end
 end
+
 
 # Creates instance of Board class
-game = Board.new
-game.show_board
-puts game.row_two
+lets_start = Game.new
+lets_start.play('O', 2, 1)
 
-# Class for O Pieces
-class Pieceoh < Board
-  attr_accessor :array_num, :space_num, :display
-
-  include Placement
-  def initialize(array_num, space_num)
-    @array_num = array_num
-    @space_num = space_num
-    @display = 'O'
-    super()
-  end
-end
-
-# Class for X Pieces
-class Piecex < Board
-  attr_accessor :array_num, :space_num, :display
-
-  def initialize(array_num, space_num)
-    @array_num = array_num.to_i
-    @space_num = space_num.to_i
-    @display = 'X'
-    super()
-  end
-end
-
-# Run Game
-
-turn_one = Pieceoh.new(2, 3)
-puts turn_one.array_num
-puts turn_one.space_num
-puts turn_one.display
-puts turn_one.calculate_array(2)
-puts turn_one.calculate_space(3)
-
-turn_two = Piecex.new(1, 3)
-puts turn_two.array_num
-puts turn_two.space_num
-puts turn_two.display
